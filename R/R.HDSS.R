8a54878578670c55da05f2eabe8eddd8075fed7a
Alexander Ploner <alexander.ploner@ki.se>
2014-08-18 17:58:43 +0200

    Changed package dcoumentation to roxygen2 format

---
+++ R/R.HDSS.R
@@ -0,0 +1,15 @@
+#' Process HDSS demographic data from the INDEPTH project
+#'
+#' The INDEPTH project collects data in numerous health and demographic
+#' surveillance system (HDSS) field sites in Africa, Asia and Oceania. This
+#' package provides methods for processing the data files distributed through
+#' the INDEPTH website.
+#'
+#' Note that while this package has been written to facilitate an existing
+#' collaboration within the INDEPTH project, it should not be considered part of
+#' INDEPTH. It is an independent effort to make available tools for processing
+#' and analysing the data distributed through the project. 
+#'
+#' @docType package
+#' @name R.HDSS
+NULL
