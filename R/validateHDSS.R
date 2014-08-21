5475aa5abea1c4dffd9e5c49d7bb14ab8cbbdc56
Alexander Ploner <alexander.ploner@ki.se>
2014-08-21 18:39:45 +0200

    Started of validation of verbal autopsy variables

--- R/validateHDSS.R
+++ R/validateHDSS.R
@@ -35,9 +35,8 @@ validateHDSS = function(x, clean=TRUE, inclVA=TRUE, verb=TRUE)
 	isGood = isGood & test
 
 	## Check for valid non-missing centre identifiers of length 5
-	test = varExists("CentreId", x, verb=verb)
+	test = varExistsNoMissing("CentreId", x, verb=verb)
 	if (test) {
-		test = test & varNoMissing("CentreId", x, verb=verb)
 		if ( any(nchar(as.character(x$CentreID)) != 5) ) {
 			test = FALSE
 			if (verb) warning("Invalid code length in variable 'CentreId'")
@@ -46,14 +45,10 @@ validateHDSS = function(x, clean=TRUE, inclVA=TRUE, verb=TRUE)
 	isGood = isGood & test
 
 	## Check for non-missing subject Ids
-	test = varExists("IndividualId", x, verb=verb)
-	if (test) {
-		test = test & varNoMissing("IndividualId", x, verb=verb)
-	}
-	isGood = isGood & test
+	isGood = isGood & varExistsNoMissing("IndividualId", x, verb=verb)	
 
 	## Check for country identifier: present and valid
-	test = varExists("CountryId", x, verb=verb)	
+	test = varExistsNoMissing("CountryId", x, verb=verb)	
 	if (test) {
 		require(ISOcodes)
 		data(ISO_3166_1)
@@ -65,59 +60,40 @@ validateHDSS = function(x, clean=TRUE, inclVA=TRUE, verb=TRUE)
 	isGood = isGood & test
 
 	## Check for non-missing location Ids
-	test = varExists("LocationId", x, verb=verb)
-	if (test) {
-		test = test & varNoMissing("LocationId", x, verb=verb)
-	}
-	isGood = isGood & test
+	isGood = isGood & varExistsNoMissing("LocationId", x, verb=verb)	
 
 	## Check for non-missing date of birth
-	test = varExists("DoB", x, verb=verb)
-	if (test) {
-		test = test & varNoMissing("DoB", x, verb=verb)
-	}
-	isGood = isGood & test
+	isGood = isGood & varExistsNoMissing("DoB", x, verb=verb)	
 
 	## Check for non-missing event code from given list
-	test = varExists("EventCode", x, verb=verb)
+	test = varExistsNoMissing("EventCode", x, verb=verb)
 	if (test) {
-		test = test & varNoMissing("EventCode", x, verb=verb)
-		code = c("BTH", "ENU", "IMG", "OMG", "EXT", "ENT", "DTH", "DLV",
-		         "OBE", "OBL", "OBS")
-		if ( !all(as.character(x$EventCode) %in% code) ) {
-			test = FALSE
-			if (verb) warning("Unrecognized code in variable 'EventCode'")
-		}
+		test = test & varValidValues("EventCode", x, val=INDEPTH_eventCodes, verb=verb)
 	}
 	isGood = isGood & test
 
 	## Check for non-missing event date
-	test = varExists("EventDate", x, verb=verb)
-	if (test) {
-		test = test & varNoMissing("EventDate", x, verb=verb)
-	}
-	isGood = isGood & test
+	isGood = isGood & varExistsNoMissing("EventDate", x, verb=verb)
 
 	## Check for non-missing observation date
-	test = varExists("ObservationDate", x, verb=verb)
-	if (test) {
-		test = test & varNoMissing("ObservationDate", x, verb=verb)
-	}
-	isGood = isGood & test
+	isGood = isGood & varExistsNoMissing("ObservationDate", x, verb=verb)	
 
 	## Check for non-missing count of events
-	test = varExists("EventCount", x, verb=verb)
-	if (test) {
-		test = test & varNoMissing("EventCount", x, verb=verb)
-	}
-	isGood = isGood & test
+	isGood = isGood & varExistsNoMissing("EventCount", x, verb=verb)	
 
 	## Check for non-missing event number (sequential within subject?)
-	test = varExists("EventNr", x, verb=verb)
-	if (test) {
-		test = test & varNoMissing("EventNr", x, verb=verb)
+	isGood = isGood & varExistsNoMissing("EventNr", x, verb=verb)
+
+	## Only if we are reuired to test for verbal autospy results
+	if (inclVA) {
+
+		##isGood = isGood & varExists("
+
+
+
+
 	}
-	isGood = isGood & test
+	
 
 	if (clean) return(x) else return(isGood)
 
