b99dba3d3a13913ff8deee61751d020bd453390e
Alexander Ploner <alexander.ploner@ki.se>
2014-08-27 13:21:16 +0200

    Added summary function for HDSS data sets and lists of HDSS data sets

---
+++ R/summaryHDSS.R
@@ -0,0 +1,52 @@
+#' Summarize a pre-processed HDSS data set
+#'
+#' Summarize key parameters of a pre-processed HDSS data set.
+#'
+#' @param x The pre-processed HDSS data frame to be summarized
+#'
+#' @details \code{summarizeHDSS} works for a single HDSS data set,
+#'    \code{overviewHDSS} for a list of pre-processed data sets. Assuming that
+#'    the list of HDSS files is based on raw data files following the INDEPTH
+#'    naming conventions and read in by \code{readRawHDSS} before pre-processing,
+#'    \code{overviewHDSS} tires to guess at the version of the data.
+#'
+#' @return For \code{summarizeHDSS} a named list of parameters, for
+#'    \code{overviewHDSS} a data frame with centres/data files as rows and
+#'    the parameters as columns.
+#'
+#' @name summarize-HDSS
+#' @seealso \code{\link{preprocHDSS}}
+#' @export
+summarizeHDSS = function(x)
+{
+	numEvents = nrow(x)
+	numSubj   = length(unique(x$IndividualId))
+	dateRange = range(x$EventDate, na.rm=TRUE)
+	ageRange  = range(round(as.vector(x$EventDate - x$DoB)/365.25,1), na.rm=TRUE)
+	numDeaths = length(which(x$EventCode=="DTH"))
+	numBirths = length(which(x$EventCode=="BTH"))
+	vaVars = as.vector(outer(c("Cause", "Likelihood"), 1:3, paste, sep=""))
+	hasVA     = all(vaVars %in% colnames(x))
+	list("numSubjects" = numSubj, "numEvents" = numEvents,
+	     "minDate" = dateRange[1], "maxDate" = dateRange[2],
+	     "minAge"  = ageRange[1],  "maxAge"  = ageRange[2],
+	     "numBirths" = numBirths, "numDeaths" = numDeaths,
+	     "hasVerbalAutopsy" = hasVA)
+}	     
+	     
+#' @rdname summarize-HDSS
+#' @param l a list of pre-processed HDSS data files
+#' @export
+overviewHDSS = function(l)
+{
+	ll = lapply(l, function(x) as.data.frame(summarizeHDSS(x)))
+	nn = length(ll)
+	ret = ll[[1]]
+	if (nn > 1) {
+		for (i in 2:nn) {
+			ret = rbind(ret, ll[[i]])
+		}
+	}
+	rownames(ret) = names(ll)
+	ret
+}
